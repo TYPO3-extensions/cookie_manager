ff02fa61422d8b3b17b9ed2946ff7cf00f68f88d
Patrick Broens <patrick@patrickbroens.nl>
2013-02-15 10:35:20 +0100

    [BUGFIX] Javascript file CreateCookie.js is using deprecated jQuery method
    On line 2 in the file CreateCookie.js the method live() is used. As of JQuery 1.7, the live() method is deprecated.
    
    This patch changes this by replacing the method with on() to attach the click event handler.
    
    Fixes: #45480
    Releases: 1.0
    Change-Id: I10d0f0012093faeb07259fd792882ca8050525bd
    Reviewed-on: https://review.typo3.org/18294
    Reviewed-by: Patrick Broens
    Tested-by: Patrick Broens

--- Resources/Public/Javascripts/CreateCookie.js
+++ Resources/Public/Javascripts/CreateCookie.js
@@ -1,8 +1,8 @@
-$('document').ready(function(){
-	$('.createCookieAction a').live('click', function(e){
+$('document').ready(function() {
+	$('.createCookieAction a').on('click', function(e) {
 		e.preventDefault();
 		$.getJSON($(this).attr('href'), function(data) {
-				$('.createCookieAction').html(data.msg);
-			});
+			$('.createCookieAction').html(data.msg);
+		});
 	});
 });
